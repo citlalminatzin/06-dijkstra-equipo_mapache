bacbd86869978c9fd9ce7bc06991bb33555c472d
Alba420p <albapaulina150@gmail.com>
2026-03-23 12:59:14 -0600

    Update main.py
    cambios en funciones de dijkstra

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     #Ejercicio 1
     print("\nEjercicio 1")
     print("\n" + "-"*20)
-    ej1 = ejercicio_1()
+    ej1 = dijkstra.ejercicio_1()
     print(f"Nodo inicial: 0")
     print(f"Distancias finales (D): {ej1[0]}")
     print(f"Predecesores (P): {ej1[1]}")
@@ -17,7 +17,7 @@ def main():
     destino=2
 
     distancias, predecesores= ej1
-    camino = cam_opt(predecesores, origen, destino)
+    camino = dijkstra.cam_opt(predecesores, origen, destino)
 
     if camino:
         print(f"Camino óptimo de {origen} a {destino}:",
@@ -30,23 +30,23 @@ def main():
     print("\nEjercicio 3:")
     print("\n" + "-"*20)
     print("\nGráfica 1")
-    ej3a = ejercicio_3a()
+    ej3a = dijkstra.ejercicio_3a()
     print(f"Distancias desde el nodo 0: {ej3a[0][0]}")
     print(f"Predecesores desde el nodo 0: {ej3a[0][1]}")
 
     print("\nGráfica 2")
-    ej3b = ejercicio_3b()
+    ej3b = dijkstra.ejercicio_3b()
     print(f"Distancias desde el nodo 0: {ej3b[0][0]}")
     print(f"Predecesores desde el nodo 0: {ej3b[0][1]}")
 
     print("\nGráfica 3")
-    ej3c = ejercicio_3c()
+    ej3c = dijkstra.ejercicio_3c()
     print(f"Distancias desde el nodo 0: {ej3c[0][0]}")
     print(f"Predecesores desde el nodo 0: {ej3c[0][1]}")
 
     #Ejercicio 4
     print("Ejercicio 4 ")
-    print(f"Distancia mínima  {ejercicio_4()}")
+    print(f"Distancia mínima  {dijkstra.ejercicio_4()}")
 
 
 if __name__ == "__main__":
